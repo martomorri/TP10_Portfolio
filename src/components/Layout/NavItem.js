12fa13fd2aefbcb313e620a061a9c6ffd5e1370f
Martin Morrison <martomorr@gmail.com>
2023-11-15 17:19:49 -0300

    added badge, only missing styles

--- src/components/Layout/NavItem.js
+++ src/components/Layout/NavItem.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { string, object, oneOfType } from 'prop-types'
 import "./NavItem.css";
+import { favContext } from "../../context/favContext";
+import Badge from "@mui/material/Badge";
 
 function NavItem({ item, path = "" }) {
+  const { favProj } = useContext(favContext)
   const size = item === '☆' ? "28px" : "16px"
-  const className = item === '☆' ? "" : "btn btn-light"
+  const className = item === '☆' ? "m-0" : "btn btn-light"
+  const badge = item === '☆' ? favProj.length : 0
+
   return (
     <li className="nav-item">
-      <a style={{fontSize: size, textDecoration: "none", color: "black"}} className={className} href={path}>
-        {item}
-      </a>
+      <Badge id="badge" color="primary" badgeContent={badge}>
+        <a style={{ fontSize: size, textDecoration: "none", color: "black" }} className={className} href={path}>
+          {item}
+        </a>
+      </Badge>
     </li>
   );
 }
