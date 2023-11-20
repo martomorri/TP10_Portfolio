64ecd3862dccca4e727e70c78d770402d9350513
Martin Morrison <martomorr@gmail.com>
2023-11-19 22:10:01 -0300

    terminado

--- src/pages/js/Layout.js
+++ src/pages/js/Layout.js
@@ -1,8 +1,5 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-// import Home from "./Home";
-// import Skills from "./Skills";
-// import Contacto from "./Contacto";
 import Nav from "../../components/Layout/Nav";
 
 function Layout() {
